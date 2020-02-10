a4ed733966502c0bd6db026ed8aa0d54858f67e9
Tomas Malbran <malbrantomas@gmail.com>
2020-02-10 20:00:47 -0300

    Commands utils, new scripts, and several other updates

--- index.js
+++ index.js
@@ -1,43 +1,52 @@
 #!/usr/bin/env node
 
-const output = require("./lib/utils/output");
+const Output = require("./lib/utils/output");
+const Config = require("./lib/utils/config");
 
-const fs     = require("fs");
-const path   = require("path");
-
-const clear  = require("clear");
-const chalk  = require("chalk");
-const figlet = require("figlet");
+const FS     = require("fs");
+const Path   = require("path");
+const Clear  = require("clear");
 
 
 
-clear();
+/**
+ * The Main Function
+ * @returns {Promise}
+ */
+async function main() {
+    Clear();
+    Output.logo("Runner");
 
-console.log(chalk.yellow(
-    figlet.textSync("Runner", { horizontalLayout : "full" })
-));
+    const currentPath = process.cwd();
+    const basePath    = Path.basename(currentPath);
+    if (FS.existsSync(`${basePath}/runner.json`)) {
+        Output.exit("You must have a runner.json file");
+    }
 
-const currentPath = process.cwd();
-const basePath    = path.basename(currentPath);
-if (fs.existsSync(`${basePath}/runner.json`)) {
-    output.exit("You must have a runner.json file");
+    const scriptName = process.argv[2];
+    if (!scriptName) {
+        Output.exit("You must provide a script name");
+    }
+
+    const scriptPath = Path.join(__dirname, "lib", "script", scriptName);
+    const scriptFile = FS.readFileSync(Path.join(__dirname, "scripts.json"));
+    const scriptData = JSON.parse(scriptFile.toString());
+
+    if (scriptData && scriptData[scriptName]) {
+        let configData = {};
+        try {
+            const configFile = FS.readFileSync(`${currentPath}/runner.json`);
+            configData = JSON.parse(configFile.toString());
+        } catch (e) {
+            Output.exit("The runner JSON is invalid");
+        }
+
+        const configScript = configData[scriptName];
+        const params       = process.argv.slice(3);
+
+        const [ config, args ] = await Config.parse(scriptData[scriptName], configScript, params);
+        require(scriptPath)(config, args);
+    }
 }
 
-const scriptName = process.argv[2];
-if (!scriptName) {
-    output.exit("You must provide a script name");
-}
-
-const scriptPath = path.join(__dirname, "lib", "script", scriptName);
-const scriptFile = fs.readFileSync(path.join(__dirname, "scripts.json"));
-const scriptData = JSON.parse(scriptFile.toString());
-
-if (scriptData && scriptData[scriptName]) {
-    const configFile   = fs.readFileSync(`${currentPath}/runner.json`);
-    const configData   = JSON.parse(configFile.toString());
-    const configScript = configData[scriptName];
-    const params       = process.argv.slice(3);
-
-    const [ config, args ] = Config.parse(scriptData[scriptName], configScript, params);
-    require(scriptPath)(config, args);
-}
+main();
